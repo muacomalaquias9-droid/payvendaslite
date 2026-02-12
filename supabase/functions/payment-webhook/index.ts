e388a56bdb47e95e87c7642515390910c834c3f4
muacocine <rankigdev@gmail.com>
2026-02-12 23:42:19 +0100

    Update index.ts

--- supabase/functions/payment-webhook/index.ts
+++ supabase/functions/payment-webhook/index.ts
@@ -8,7 +8,7 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
 
-const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
+const supabaseUrl = Deno.env.get("https://owtcqvefxjncwunhkxdc.supabase.co")!;
 const supabaseServiceKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
